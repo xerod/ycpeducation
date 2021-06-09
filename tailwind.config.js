d4b584b6759021f4b02d56f48c196b5bfa73c35d
Akramurridjal Rahman <xerod@outlook.com>
2021-06-09 19:32:25 +0700

    ✨ Add 'bg-mask-' utilities to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const windmill = require("@windmill/react-ui/config");
 const colors = require("tailwindcss/colors");
+const plugin = require("tailwindcss/plugin");
 
 module.exports = windmill({
   mode: "jit",
@@ -21,10 +22,29 @@ module.exports = windmill({
       backgroundSize: {
         overflow: "250% 100%",
       },
+      zIndex: {
+        "-10": "-10",
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        ".bg-mask-t-0": {
+          maskImage:
+            "linear-gradient(to top, rgba(0, 0, 0, 1.0) 0%, transparent 100%)",
+        },
+        ".bg-mask-b-0": {
+          maskImage:
+            "linear-gradient(to bottom, rgba(0, 0, 0, 1.0) 0%, transparent 100%)",
+        },
+      };
+
+      addUtilities(newUtilities);
+    }),
+  ],
 });
