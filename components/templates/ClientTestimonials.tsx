4e201223cd218c3d437502affa7255c36cab6533
Akramurridjal Rahman <xerod@outlook.com>
2021-06-11 02:11:18 +0700

    ✨ Client Testimonials section responsiveness done

--- components/templates/ClientTestimonials.tsx
+++ components/templates/ClientTestimonials.tsx
@@ -4,16 +4,19 @@ import QuoteIcon from "../elements/QuoteIcon";
 
 export default function ClientTestimonials() {
   return (
-    <div className="relative py-32 px-16 bg-white">
+    <div className="relative py-32 bg-white lg:px-10 xl:px-16">
       <Header className="text-center">Client Testimonials</Header>
-      <div className="mt-8 px-32">
-        <QuoteIcon height="96" className="-mb-16 text-amber-100" />
-        <p className="text-3xl leading-9 font-extralight text-center text-gray-800">
+      <div className="px-4 mt-8 lg:px-32">
+        <QuoteIcon
+          height="96"
+          className="-mb-16 text-orange-50 lg:text-amber-100"
+        />
+        <p className="text-2xl leading-7 text-left text-gray-800 lg:text-center lg:leading-9 lg:text-3xl font-extralight">
           “Our child has truly enjoyed himself in the camp! He gained more
           confidence in speaking English and has taken an interest in robotics
           and coding after the camp”
         </p>
-        <p className="mt-4 text-xl leading-7 font-medium text-center text-gray-600">
+        <p className="mt-4 text-base font-medium leading-6 text-left text-gray-600 lg:leading-7 lg:text-center lg:text-xl">
           From the parent of a participant of B2C Summer Camp
         </p>
       </div>
